f778cc7af077bd04437574f77d5586e8d44f7115
Colin Viebrock <colin@viebrock.ca>
2014-03-20 15:33:00 -0500

    migration

---
+++ app/database/migrations/2014_03_20_203143_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('title', 50);
+			$table->text('text')->nullable();
+			$table->boolean('active')->default(true);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
